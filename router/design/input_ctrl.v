a633ce34e113e837d5c361f5bd0abe5fa13deed2
Takeru Nishimura <tnishimu@viterbi-scf2.usc.edu>
2026-03-16 22:00:00 -0700

    completed input/output_ctrl with passing testbench

--- router/design/input_ctrl.v
+++ router/design/input_ctrl.v
@@ -7,10 +7,9 @@ input clk, reset;
 input send_in; //handshaking signal, neighbor is ready to send a packet to you
 input [63:0] data_in; //data input
 input polarity; //0=even or 1=odd
-//grants from output_ctrl
-input grant_N, grant_S, grant_E, grant_W, grant_PE;
-//requests to output_ctrl
-output forward_N, forward_S, forward_E, forward_W, forward_PE;
+input grant_N, grant_S, grant_E, grant_W, grant_PE; //grants from output_ctrl
+
+output forward_N, forward_S, forward_E, forward_W, forward_PE; //requests to output_ctrl
 output ready_in; //handshaking signal, you are ready to accept packet
 output [63:0] data_out;
 
@@ -20,6 +19,10 @@ reg even_buffer_full, odd_buffer_full;
 
 wire [3:0] hopX, hopY;
 wire dirX, dirY;
+/*
+dirX = 1(East), 0(West)
+dirY = 1(North), 0(South)
+*/
 wire [63:0] current_buffer;
 wire buffer_has_data;
 
@@ -57,13 +60,11 @@ always @(posedge clk) begin
             odd_buffer <= data_in;
             odd_buffer_full <= 1'b1;
         end
-        if (polarity == 0 && (grant_N || grant_E || grant_S || grant_W || grant_PE))
-            odd_buffer_full <= 0;
-        if (polarity == 1 && (grant_N || grant_E || grant_S || grant_W || grant_PE))
+        // if there are any grant signals, that indicates data from input_ctrl is sent to output_ctrl, so the buffer in input_ctrl side is empty
+        if (polarity == 0 && (grant_N || grant_E || grant_S || grant_W || grant_PE)) 
             even_buffer_full <= 0;
+        if (polarity == 1 && (grant_N || grant_E || grant_S || grant_W || grant_PE))
+            odd_buffer_full <= 0;
     end
-    
-
 end
-
 endmodule
